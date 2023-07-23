abde870b1579df509de8d3add0157dd4b70aed67
Arunabh Ghosh <arunabh.tech@gmail.com>
2023-07-23 19:35:16 +0530

    Updated the Heading and Slug

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ const Home = () => {
 	return (
 		<section className="w-full flex-center flex-col">
 			<h1 className="head_text text-center">
-				Primary Slug
+				Main Heading
 				<br className="max-md:hidden" />
-				<span className="orange_gradient text-center">Small Description</span>
+				<span className="orange_gradient text-center">Slug</span>
 			</h1>
 			<p className="desc text-center">
 				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quos
